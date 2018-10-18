c02a3e828b71accd30b45eae2dcbc9796ae99c2d
JustenP <phelps.justen@gmail.com>
2018-10-18 01:32:06 -0700

    added contributions folder and Justen-Phelps.js

---
+++ contributions/Justen-Phelps.js
@@ -0,0 +1,7 @@
+console.log(
+"\n"+
+"I became a dev because I absolutely love to code and solve problems. I have always loved computers and technology, and it was a natural fit for me!"+
+"\n"+
+"\n"+
+"-Justen Phelps"                                                                                                                                                                                                                                                                                                                                                                                  
+)
