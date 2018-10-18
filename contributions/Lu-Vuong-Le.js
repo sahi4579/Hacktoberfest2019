859859e5af025d0bce8610dfd0d8f3a546cc72ff
luvuong-le <lu-vuongle@hotmail.com>
2018-10-18 21:08:27 +1100

    Added my reason

---
+++ contributions/Lu-Vuong-Le.js
@@ -0,0 +1,5 @@
+const name = 'Lu-Vuong Le';
+const age = '21';
+const goal = 'Full Stack Developer'
+
+console.log(`Hey all! Thought I'd share my reason too. My name is ${name} and I'm a ${age} developer in Melbourne. I became passionate about software development a little bit after I graduated and ever since then the passion has just kept rising! My goal is to become a successful ${goal} someday. That's just a little bit of my story, keep learning and keep hacking everyone! Happy Hacktoberfest 2018!!`)
