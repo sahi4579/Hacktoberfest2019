a2a1ac53eb8a9a8ab6670595295c1c3e1977a521
Justen Phelps <phelps.justen@gmail.com>
2018-10-18 23:11:00 -0700

    Fixed misplaced program file

--- Frank-Carlone.js
+++
@@ -1,8 +0,0 @@
-const name = 'Frank Carlone';
-const message = 'I am currently studying to become a software developer. My goal is to have a programming career and build some interesting applications.'
-
-const contribution = (name, message) => {
-  console.log(`Hello.  My name is ${name}.  ${message}`);
-};
-
-contribution(name, message);
