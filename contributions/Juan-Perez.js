8b28aa04719dada4a9b6dfc4d825d41f2d72c101
Juan Perez <juanmarcperez@gmail.com>
2018-10-31 22:00:14 -0500

    little bio about me

---
+++ contributions/Juan-Perez.js
@@ -0,0 +1,5 @@
+const name = 'Juan Perez';
+const age = '28';
+const goal = 'Full Stack Developer'
+
+console.log(`Hola! My name is ${name} and I'm ${age} yrs. old, I am currently not working as a developer but I am in the process of expanding my portfolio to become one. I always loved working with computer and was introduced to coding in college, I am currently a Senior almost done with school and I still love coding! My goal is to become a ${goal} someday. Thanks for reading!`)
