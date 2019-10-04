3c634c7a5aab17e3a5a50b987136ac6be2edda7b
Priyanka Rajput <priyankarajput3010@gmail.com>
2019-10-05 01:48:49 +0530

    Update and rename Priyanka.js to Priyanka.java

---
+++ contributions/Priyanka.java
@@ -0,0 +1,7 @@
+class Priyanka{
+ public static void main(String args[]){
+   System.out.println("Hello World!");
+   System.out.println("My name is Priyanka Rajput");
+   System.out.println("I became a Developer to solve the problems around me by using latest technologies and to grow my knowledge.");
+ }
+}
