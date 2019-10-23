0062c23b76134ef8d850aa4d6a73ae2573204dd4
ravikishorethella <ravikishorethella@gmail.com>
2019-10-23 00:09:48 -0400

    added sahi details

---
+++ contributions/sahi.js
@@ -0,0 +1,8 @@
+const name = 'Sahitya Ambati';
+const message = 'I am currently working as a web developer.'
+
+const info = (name, message) => {
+    console.log(`Hello.  My name is ${name}.  ${message}`);
+};
+
+info(name, message);
